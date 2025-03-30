bf5ef51a38b23d1a1a98d1061b121dff024770e0
junhopark97 <jh97.dev@gmail.com>
2025-03-30 23:45:50 +0900

    feat: define Post and PostCreate models

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from src.api.health import health_router
+from src.api.post import post_router
 
 app = FastAPI()
 app.include_router(health_router, prefix="/api")
+app.include_router(post_router, prefix="/api")
