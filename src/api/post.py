db64956b882a4b4e33d1106ac6d94b2856e1f10c
ydarh <110903858+f-lab-ydrah@users.noreply.github.com>
2025-04-15 11:21:13 +0900

    Update post.py

--- src/api/post.py
+++ src/api/post.py
@@ -15,7 +15,8 @@ class PostResponse(BaseModel):
     author: str
     title: str
     content: str
-    created_at: datetime  # TODO: 시각에 대한 표현방법이 어떤 것들이 있는지. string, int을 사용 방식 -> 언제 내가 사용해야 하는지?
+    # TODO: 시각에 대한 표현방법이 어떤 것들이 있는지 알아보기. (ISO-8601, unix timestamp 등)
+    created_at: datetime
 
 
 class PostRequest(BaseModel):
@@ -33,14 +34,13 @@ async def create_post(
     post: PostRequest, x_author: str = Header(..., alias="X-Author")
 ) -> PostResponse:
     new_post = PostResponse(
-        # TODO: uuid, ulid 써보기.
-        # NOTE: id를 만드는 방식 AUTO INCREMENT, uuid 이게 뭔지, 각 장단점
-        # ulid 는 왜? 언제? 어떤 문제를 해결하는지
+        # TODO: uuid 대신 ulid 써보기.
+        # TODO: id를 만드는 여러 방식에 대해 알아보기.
         id=str(uuid4()),
         author=x_author,
         title=post.title,
         content=post.content,
-        created_at=datetime.now(TIME_ZONE),
+        created_at=datetime.now(TIME_ZONE),  # TODO: aware datetime vs naiva datetime 에 대해 알아보기
     )
     posts.append(new_post)
     return new_post
@@ -60,8 +60,8 @@ async def get_posts() -> list[PostResponse]:
 )
 async def get_post(post_id: str) -> PostResponse:
     post = next((p for p in posts if p.id == post_id), None)
-    if post is None:
-        raise HTTPException(status_code=404, detail="Post not found")  # TODO: 404 fastapi.status <- 사용해보기
+    if post is None:  # TODO: post is None vs not post 중 어떤게 더 좋은 방법일까? 알아보기
+        raise HTTPException(status_code=404, detail="Post not found")  # TODO: fastapi.status 사용하기. 그리고 이게 왜 더 좋은지 설명하기
     return post
 
 
@@ -71,20 +71,20 @@ async def get_post(post_id: str) -> PostResponse:
     status_code=status.HTTP_200_OK,
 )
 async def update_post(
-    post_id: str, post_data: PostRequest, x_author: str = Header(..., alias="X-Author")  # TODO: 요청 헤더에 이름을 붙이는 컨벤션들 뭐가 있을까?
+    post_id: str, post_data: PostRequest, x_author: str = Header(..., alias="X-Author")  # TODO: 요청 헤더에 이름을 붙이는 컨벤션들은 뭐가 있는지 알아보기. ex. X-는 왜 붙일까?
 ) -> PostResponse:
     post_index = next((i for i, p in enumerate(posts) if p.id == post_id), None)
 
     if post_index is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id {repr(post_id)} not found.",
+            detail=f"Post with id {repr(post_id)} not found.",  # TODO: 에러 메시지는 어떻게 작성하는 것이 좋은지 알아보기
         )
 
     post = posts[post_index]
     if post.author != x_author:
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,  # TODO: 4XX 에러 다 정리하기 (401, 403, 404, 405, 409 설명 할 수 있어야 함)
+            status_code=status.HTTP_403_FORBIDDEN,  # TODO: 2XX, 4XX 에러 알아보기 (401, 403, 404, 405, 409 설명 할 수 있어야 함)
             detail="Access denied: not the post owner.",
         )
 
@@ -113,7 +113,7 @@ async def delete_post(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id '{repr(post_id)}' not found.",
         )
-
+    # TODO: 나는 언제 개행을 사용하는지 일관된 규칙 설명할 수 있기 (ex. 언제 어디서 코드에 개행을 넣으시는 건가요? 기준이 있나요?)
     post = posts[post_index]
 
     if post.author != x_author:
@@ -124,4 +124,5 @@ async def delete_post(
 
     del posts[post_index]
 
+    # TODO: 함수의 반환 값으로 dict를 사용하는게 왜 좋지 않은지 설명하기. 그리고 대안 찾아보고 적용해보기
     return {"message": f"Post '{repr(post_id)}' deleted successfully."}
