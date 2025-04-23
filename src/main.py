9e2a91745cd5b3c8cceff2201bef74653cfa8fa7
junhopark97 <jh97.dev@gmail.com>
2025-04-23 21:58:48 +0900

    feat: add CRUD functionality for posts using SQLModel and SQLite3

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from fastapi.concurrency import asynccontextmanager
+from sqlmodel import Session
 
 from src.api.health import health_router
 from src.api.post import post_router
+from src.sqlite3.connection import get_session, init_db
 
-app = FastAPI()
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(health_router, prefix="/api")
 app.include_router(post_router, prefix="/api")
