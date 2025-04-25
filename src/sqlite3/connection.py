3c42c9daa37d98977619335d00d44847c9c93fc8
ydarh <110903858+f-lab-ydrah@users.noreply.github.com>
2025-04-25 11:32:41 +0900

    Update connection.py

--- src/sqlite3/connection.py
+++ src/sqlite3/connection.py
@@ -5,6 +5,7 @@ DATABASE_URL = f"sqlite:///{sqlite_file_name}"
 
 # TODO: 커넥션 풀에 대해 설명하기.
 # TODO: create_engine의 주요 파라미터들에 대해 설명하기.
+# TODO: 현재 주입받는 엔드포인트 함수들은 비동기(async)로 되어있는데, DB는 sync 엔진으로 되어 있음. 어떤 문제가 있는지 설명하고 적절하게 수정하기.
 ENGINE = create_engine(
     DATABASE_URL,
     echo=True,
