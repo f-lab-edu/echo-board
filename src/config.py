c8023147f20d08e5c4a948c6c7a2376a482513c3
ydarh <110903858+f-lab-ydrah@users.noreply.github.com>
2025-04-25 11:33:05 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
+# TODO: 파이썬에서 설정 파일을 다루는 여러 방식들에 대해 설명하기
 class ServerConfig(BaseSettings):
     host: str = Field(default="127.0.0.1", alias="DEV_HOST")
     port: int = Field(default=8000, alias="DEV_PORT")
