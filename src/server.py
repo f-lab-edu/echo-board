e2fa12fece695d8de956e098a3c59be0c7ff880f
ydarh <110903858+f-lab-ydrah@users.noreply.github.com>
2025-06-07 16:38:56 +0900

    Update server.py

--- src/server.py
+++ src/server.py
@@ -2,6 +2,6 @@ import uvicorn
 
 from src.config import dev
 
-
+# COMMENT: 보통 이 로직은 main.py 내 __main__ 안에 두곤 합니다.
 def run():
     uvicorn.run("src.main:app", host=dev.host, port=dev.port, reload=True)
